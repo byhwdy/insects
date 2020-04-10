51449484ce676c719afb2df8eac63230826e84c2
zhengshaohua <zhengshaohua@58.com>
2020-04-10 18:15:28 +0800

    v1 edit

--- tools/train.py
+++ tools/train.py
@@ -99,7 +99,7 @@ def main():
     save_dir = os.path.join(cfg.save_dir, cfg_name)
     best_box_ap_list = [0.0, 0]
     if FLAGS.use_vdl:
-        log_writter = LogWriter(FLAGS.vdl_dir, sync_cycle=5)
+        log_writter = LogWriter(FLAGS.vdl_log_dir, sync_cycle=5)
         with log_writter.mode("train") as vdl_logger:
             scalar_loss = vdl_logger.scalar(tag="loss")
         with log_writter.mode("val") as vdl_logger:
