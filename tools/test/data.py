626355931817bed73c3d115decf146625e2aeb18
zhengshaohua <zhengshaohua@58.com>
2020-04-12 10:13:24 +0800

    v1_aug start

--- tools/test/data.py
+++ tools/test/data.py
@@ -7,37 +7,37 @@ sys.path.append('.')
 from ppdet.core.workspace import load_config, merge_config, create
 from ppdet.data.reader import create_reader
 
-def get_test_images(infer_dir, infer_img):
-    """
-    Get image path list in TEST mode
-    """
-    assert infer_img is not None or infer_dir is not None, \
-        "--infer_img or --infer_dir should be set"
-    assert infer_img is None or os.path.isfile(infer_img), \
-            "{} is not a file".format(infer_img)
-    assert infer_dir is None or os.path.isdir(infer_dir), \
-            "{} is not a directory".format(infer_dir)
+# def get_test_images(infer_dir, infer_img):
+#     """
+#     Get image path list in TEST mode
+#     """
+#     assert infer_img is not None or infer_dir is not None, \
+#         "--infer_img or --infer_dir should be set"
+#     assert infer_img is None or os.path.isfile(infer_img), \
+#             "{} is not a file".format(infer_img)
+#     assert infer_dir is None or os.path.isdir(infer_dir), \
+#             "{} is not a directory".format(infer_dir)
     
-    images = []
+#     images = []
 
-    # infer_img has a higher priority
-    if infer_img and os.path.isfile(infer_img):
-        images.append(infer_img)
-        return images
+#     # infer_img has a higher priority
+#     if infer_img and os.path.isfile(infer_img):
+#         images.append(infer_img)
+#         return images
 
-    infer_dir = os.path.abspath(infer_dir)
-    exts = ['jpg', 'jpeg', 'png', 'bmp']
-    exts += [ext.upper() for ext in exts]
-    for ext in exts:
-        images.extend(glob.glob('{}/*.{}'.format(infer_dir, ext)))
-    assert len(images) > 0, "no image found in {}".format(infer_dir)
+#     infer_dir = os.path.abspath(infer_dir)
+#     exts = ['jpg', 'jpeg', 'png', 'bmp']
+#     exts += [ext.upper() for ext in exts]
+#     for ext in exts:
+#         images.extend(glob.glob('{}/*.{}'.format(infer_dir, ext)))
+#     assert len(images) > 0, "no image found in {}".format(infer_dir)
 
-    # logger.info("Found {} inference images in total.".format(len(images)))
+#     # logger.info("Found {} inference images in total.".format(len(images)))
 
-    return images
+#     return images
 
 
-yml_file = 'configs/yolov3_darknet_insects.yml'
+yml_file = 'configs/yolov3_darknet_v1_aug.yml'
 # yml_file = 'configs/yolov3_mobilenet_v1_fruit.yml'
 cfg = load_config(yml_file)
 # print(cfg.TrainReader)
@@ -50,12 +50,12 @@ cfg = load_config(yml_file)
 # print(dataset.cname2cid)
 # exit()
 
-dataset = cfg.TestReader['dataset']
-infer_dir = 'dataset/insects/val/images'
-infer_img = 'dataset/insects/val/images/1998.jpeg'
-infer_img = None
-test_images = get_test_images(infer_dir, infer_img)
-dataset.set_images(test_images)
+# dataset = cfg.TestReader['dataset']
+# infer_dir = 'dataset/insects/val/images'
+# infer_img = 'dataset/insects/val/images/1998.jpeg'
+# infer_img = None
+# test_images = get_test_images(infer_dir, infer_img)
+# dataset.set_images(test_images)
 # imid2path = dataset.get_imid2path()
 # catid2name = dataset.get_cid2cname()
 # roidb = dataset.get_roidb()
@@ -65,18 +65,18 @@ dataset.set_images(test_images)
 
 
 
-# train_reader = create_reader(cfg.TrainReader)
+train_reader = create_reader(cfg.TrainReader)
 # eval_reader = create_reader(cfg.EvalReader)
-infer_reader = create_reader(cfg.TestReader)
+# infer_reader = create_reader(cfg.TestReader)
 
-# batch = next(train_reader())
+batch = next(train_reader())
 # batch = next(eval_reader())
-batch = next(infer_reader())
+# batch = next(infer_reader())
 
 
 print('batch_size: ', len(batch))
 print('one_smaple: ')
-one_smaple = batch
+one_smaple = batch[0]
 print(one_smaple)
 # print(one_smaple[0].shape)
 # print(type(batch))
