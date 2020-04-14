2a6159c4c2a91b53c3439cbffd580e275ef67cdf
zhengshaohua <zhengshaohua@58.com>
2020-04-14 22:22:10 +0800

    v2 start

--- tools/test/model.py
+++ tools/test/model.py
@@ -11,7 +11,15 @@ from ppdet.modeling.anchor_heads import yolo_head
 yml_file = 'configs/yolov3_resnet.yml'
 cfg = load_config(yml_file)
 
-model = create('ResNet')
-print(model)
+backbone = create('ResNet')
+
+im = fluid.data(name='image',
+            	shape=[None, 3, None, None],
+            	dtype='float32',
+            	lod_level=0)
+out = backbone(im)
+print(fluid.default_main_program());exit()
+print(out)
 # print(vars(model.yolo_head.yolo_loss))
 # print(vars(model.yolo_head.nms))
+
